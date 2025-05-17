8fa8720f73b7b9520d38807405ae63b104cebfc6
Federico <fedealtava1992@gmail.com>
2025-05-17 18:34:41 +0200

    feat:home page done

---
+++ src/presentation/pages/Home.tsx
@@ -0,0 +1,103 @@
+import { useState, useEffect } from "react";
+import {
+  Container,
+  Typography,
+  Card,
+  CardContent,
+  CardMedia,
+  CardActions,
+  Button,
+  CircularProgress,
+  Box
+} from "@mui/material";
+
+import Grid from '@mui/material/Grid';
+
+import { FirebaseRestaurantRepository } from "../../infrastructure/firebase/FirebaseRestaurantRepository";
+import { ListRestaurantsUseCase } from "../../application/use-cases/restaurant/ListRestaurantsUseCase";
+import { Restaurant } from "../../domain/entities/Restaurant";
+import { Link } from "react-router-dom";
+
+export const Home = () => {
+  const [restaurants, setRestaurants] = useState<Restaurant[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string>("");
+
+  useEffect(() => {
+    const load = async () => {
+      try {
+        const repo = new FirebaseRestaurantRepository();
+        const useCase = new ListRestaurantsUseCase(repo);
+        const list = await useCase.execute();
+        setRestaurants(list);
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      } catch (error) {
+        setError("Error listing restaurants");
+      } finally {
+        setLoading(false);
+      }
+    };
+    load();
+  }, []);
+
+  if (loading) {
+    return (
+      <Box display="flex" justifyContent="center" mt={10}>
+        <CircularProgress />
+      </Box>
+    );
+  }
+
+  if (error) {
+    return (
+      <Container maxWidth="sm">
+        <Typography color="error" align="center" mt={5}>
+          ❌ {error}
+        </Typography>
+      </Container>
+    );
+  }
+
+  return (
+    <Container maxWidth="lg" sx={{ mt: 5 }}>
+      <Typography variant="h4" gutterBottom align="center">
+        Restaurants
+      </Typography>
+
+      <Grid container spacing={4}>
+        {restaurants.map((r) => (
+          <Grid size={{xs:12, sm:6, md:4}} key={r.id.value}>
+            <Card>
+              {r.imageUrl.value && (
+                <CardMedia
+                  component="img"
+                  height="140"
+                  image={r.imageUrl.value}
+                  alt={r.name.value}
+                />
+              )}
+              <CardContent>
+                <Typography variant="h6">{r.name.value}</Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Category: {r.category.value}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Rating: {r.rating.value.toFixed(1)}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button
+                  size="small"
+                  component={Link}
+                  to={`/restaurant/${r.id.value}`}
+                >
+                  View Menu
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
+};
--- src/presentation/routes/AppRouter.tsx
+++ src/presentation/routes/AppRouter.tsx
@@ -1,12 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { RegisterUser } from "../pages/RegisterUser";
-
+import { RegisterRestaurant } from "../pages/RegisterRestaurant";
+import { Home } from "../pages/Home";
 
 export const AppRouter = () => {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/register" element={<RegisterUser />} />
+        <Route path="/register-restaurant" element={<RegisterRestaurant />} />
       </Routes>
     </Router>
   );
